75385d1966eab024927d46e933e519c9795631c6
Yaroslav Youshchenko <yaroslav.yushchenko1@gmail.com>
2021-12-16 17:42:00 +0200

    : IEntityTypeConfiguration<Transaction>

---
+++ src/Infrastructure/Persistence/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infrastructure.Persistence.Configurations;
+
+internal class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.Ignore(e => e.DomainEvents);
+
+        builder.Property(t => t.Created)
+            .HasMaxLength(200)
+            .IsRequired();
+    }
+}
