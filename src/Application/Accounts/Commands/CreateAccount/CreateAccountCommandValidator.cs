8adf206d7ff406573ecccbdbfc43bf78a8c84139
Yaroslav Youshchenko <yaroslav.yushchenko1@gmail.com>
2021-12-28 02:41:12 +0200

    m

--- src/Application/Accounts/Commands/CreateAccount/CreateAccountCommandValidator.cs
+++ src/Application/Accounts/Commands/CreateAccount/CreateAccountCommandValidator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchitecture.Application.Accounts.Commands.CreatePerson;
 using FluentValidation;
 
 namespace CleanArchitecture.Application.Accounts.Commands.CreateAccount;
